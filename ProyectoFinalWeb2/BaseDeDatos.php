e34fce542eeb8d8fe8588fdb4b1cb2d57b10fb6a
fez3d <soydanielmedina@gmail.com>
2018-11-19 13:31:37 -0600

    Agrege el archivo basedeDatos

---
+++ ProyectoFinalWeb2/BaseDeDatos.php
@@ -0,0 +1,26 @@
+<?php
+    function connect(){
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "publicis";
+        $conn = new mysqli($servername, $username, $password,$database);
+        
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+        return $conn;
+    }
+    
+    function agregarBaseDatos($sql){
+        $conn = connect();
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+    }
+    
+?>
