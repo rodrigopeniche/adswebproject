aaedbc878a28d6e265abbb695a295f8a99af6f63
Rudy <Rudy@Lenovo-PC>
2018-11-19 14:16:48 -0600

    ventanaVercontrataciones casi lista falta navbar

--- ProyectoFinalWeb2/VistaVerContrataciones.php
+++ ProyectoFinalWeb2/VistaVerContrataciones.php
@@ -13,39 +13,95 @@ and open the template in the editor.
     <body>
         <header>
 		<div class="wrapper">
-|			<div class ="logo">Publicis </div>
-
+|			
+                        <img src="imagenes/logo.png" alt="Logotipo de Publicis" class="logo"/>
 			<nav>
-			
-				<a href="Inicio.php"> Inicio</a>
-				<a href="Nosotros.php"> Nosotros</a>
-				<a href="Servicio.php"> Servicios</a>
-				<a href="Contacto.php"> Contacto</a>
+                            <ul>
+                                <li><a href="Inicio.php"> Inicio</a></li>
+				<li><a href="Nosotros.php"> Nosotros</a></li>
+				<li><a href="Servicio.php"> Servicios</a></li>
+				<li><a href="Contacto.php"> Contacto</a></li>
                                 <?php
-                                session_start();
-                                
-                                    if($_SESSION['tipo_usuario'] == "administrador"){
-                                        echo '<a href="VistaAdministrador.php"> Ver Anuncios</a>';
-                                    } else if ($_SESSION['tipo_usuario'] == "cliente"){
-                                        echo '<a href="VistaContratar.php"> Contratar</a>';
-                                        echo '<a href="VistaVerContrataciones.php"> Ver Mis Contrataciones</a>';
-                                    }
-                                    
-                                    if($_SESSION['inicio'] == null || $_SESSION['inicio'] == false){
-                                        echo '<a href="IniciarSesion.php"> Iniciar Sesion</a>';
-                                    } else{
-                                        echo '<a href="CerrarSesion.php"> Cerrar Sesion</a>';
-                                    }
+//                                session_start();
+//                                
+//                                    if($_SESSION['tipo_usuario'] == "administrador"){
+//                                        echo '<a href="VistaAdministrador.php"> Ver Anuncios</a>';
+//                                    } else if ($_SESSION['tipo_usuario'] == "cliente"){
+//                                        echo '<a href="VistaContratar.php"> Contratar</a>';
+//                                        echo '<a href="VistaVerContrataciones.php"> Ver Mis Contrataciones</a>';
+//                                    }
+//                                    
+//                                    if($_SESSION['inicio'] == null || $_SESSION['inicio'] == false){
+//                                        echo '<a href="IniciarSesion.php"> Iniciar Sesion</a>';
+//                                    } else{
+//                                        echo '<a href="CerrarSesion.php"> Cerrar Sesion</a>';
+//                                    }
                                 ?>
-
+                            </ul>
 			</nav>
 	</div> 
 
-
+            
+            
+            
+            
 	</header>
         
+              <div class="div1">
 
-        
+                <label for="table"> Carteleras</label>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Clave Usuario</th>
+                            <th>clave anuncio</th>
+                            <th>Fecha Inicio</th>
+                            <th>Fecha Final</th>
+                            <th>No. de Contrato</th>
+                        </tr>
+                    </thead>
+
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                         <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                         <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                         <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                         <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                         <td></td>
+                    </tr>
+
+                </table>
+
+                <button>Cancelar Servicio</button>
+
+            </div>
         
     </body>
 </html>
