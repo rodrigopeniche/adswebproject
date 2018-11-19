71b929cea90cd5da4a9e72aeaa9a227d84a41b70
Rudy <Rudy@Lenovo-PC>
2018-11-18 19:30:03 -0600

    Ventana de administrador en proceso

---
+++ ProyectoFinalWeb2/VistaAdministrador.php
@@ -0,0 +1,140 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <link href="CssTablas.css" rel="stylesheet" type="text/css"/>
+        
+        
+        
+    </head>
+    <body>
+        <header>
+		<div class="wrapper">
+			<div class ="logo">The Masters Of Software </div>
+
+			<nav>
+			
+				<a href="#"> Inicio</a>
+				<a href="#"> Nosotros</a>
+				<a href="#"> Servicios</a>
+				<a href="Servicios"> Contacto</a>
+				<a href="Servicios"> Iniciar Sesion</a>
+
+			</nav>
+	</div> 
+
+
+	</header>
+
+       
+            <div id="div1">
+                <label for="table"> Carteleras</label>
+            <table>
+  <tr>
+    <th>id</th>
+    <th>Estacion</th>
+    <th>Titulo</th>
+    <th>Precio</th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+
+  </tr>
+  <tr>
+    <td</td>
+    <td></td>
+    <td></td>
+    <td></td>
+
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+
+  </tr>
+  
+</table>
+      </div> 
+            
+       <div id="div2">
+           <label for="table">Radio</label>
+         <table>
+  <tr>
+    <th>id</th>
+    <th>Estacion</th>
+    <th>Titulo</th>
+    <th>Precio</th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+
+  </tr>
+  <tr>
+    <td</td>
+    <td></td>
+    <td></td>
+    <td></td>
+
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+
+  </tr>
+  
+</table>       
+                
+                
+       </div>
+       
+    </body>
+</html>
+
+
