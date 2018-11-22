e1ea1a37dd5e0a9ff0686ae8da1618daf2153c3f
Rodrigo Peniche <rodrigopeniche@Rodrigos-MacBook-Pro.local>
2018-11-22 11:02:21 -0600

    Agregar clase para validar expiracion de sesion

---
+++ ValidacionSesionExpirada.php
@@ -0,0 +1,8 @@
+<?php
+    function validarSesionExpirada(){}
+        if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+            session_unset();     
+            session_destroy();  
+        }
+        $_SESSION['LAST_ACTIVITY'] = time(); 
+?>
