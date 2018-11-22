12f551c2641fea2e0c6f400f8ee71a609f7a2387
Rodrigo Peniche <rodrigopeniche@Rodrigos-MacBook-Pro.local>
2018-11-22 11:11:22 -0600

    Redirección de sesion expirada a pagina de inicio de sesion

--- ValidacionSesionExpirada.php
+++ ValidacionSesionExpirada.php
@@ -5,4 +5,7 @@
             session_destroy();  
         }
         $_SESSION['LAST_ACTIVITY'] = time(); 
+    
+        echo "Tu sesion ha expirado";
+        header('Location: IniciarSesion.php');
 ?>
