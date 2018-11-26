9e79f587fd8ec333310fdf3afba09a8e376f9d8e
Moises <Moises@192.168.1.80>
2018-11-26 12:25:17 -0600

    Elimine los enlaces a los css styleDaniel y estilos

--- ProyectoFinalWeb2/ContratarEspectaculares.php
+++ ProyectoFinalWeb2/ContratarEspectaculares.php
@@ -10,8 +10,6 @@ and open the template in the editor.
         <title>Contrato</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styleDaniel.css">
-        <link rel="stylesheet" href="estilos.css">
         <script src="contact-form-validation.js"></script>
         <script>
             function getText(element) {
