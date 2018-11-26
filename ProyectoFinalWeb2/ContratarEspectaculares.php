5b28b3c8074c78c423d6be17c631670c7334867f
Moises <Moises@192.168.1.80>
2018-11-26 12:26:26 -0600

    Agregue al enlace al header

--- ProyectoFinalWeb2/ContratarEspectaculares.php
+++ ProyectoFinalWeb2/ContratarEspectaculares.php
@@ -10,6 +10,7 @@ and open the template in the editor.
         <title>Contrato</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="HeaderStyleSheet.css">
         <script src="contact-form-validation.js"></script>
         <script>
             function getText(element) {
