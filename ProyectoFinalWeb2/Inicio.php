020113cfd44e9e2652a375d6a63e5aec454238c4
Moises <Moises@192.168.1.80>
2018-11-26 00:16:02 -0600

    Se agregó la seccion main

--- ProyectoFinalWeb2/Inicio.php
+++ ProyectoFinalWeb2/Inicio.php
@@ -79,6 +79,123 @@ and open the template in the editor.
         <img src="imagenes/imagenesInicio/banner.jpg" alt="" class="banner__img">
     	<div class="banner__content">Publicidad al alcance de un click</div>
     </section>
+    
+        <main class="main">
+    	<section class="group group--color">
+    		<div class="container">
+    			<h2 class="main__title"> Bienvenido a tu página de publicidad</h2>
+    			<p class="main__txt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+    		</div>	
+    	</section>
+
+    	<section class="group main__about__description">
+    		<div class="container container--flex">
+    			<div class="column column--50">
+    				<img src="imagenes/imagenesInicio/principal.jpg" alt="" >
+    			</div>
+    			<div class="column column--50">
+    				<h3 class="column__title">Ciudad de Mérida</h3>
+    				<p class="column__txt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+    				<a href="" class="btn btn--contact">Contact</a>
+    			</div>		
+    		</div>		
+    	</section>
+        
+        <section >
+    		<div class="contenedorMapa ">
+    			<div class="elementoMapa">
+    				<img src="imagenes/imagenesInicio/meridamap.png" alt="" onClick="link(event)">
+    			</div>	
+    		</div>		
+    	</section>
+
+    	<section class="group group--color">
+    		<div class="container">
+    			<h2 class="main__title"> Bienvenido a tu página de publicidad</h2>
+    			<p class="main__txt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+    		</div>	
+    	</section>
+        
+        <div class="contenedorRegiones">
+            <div id="Noroeste" class="contenedor  RegionesCarteleras">
+                <div class="Elemento  ">
+                            <img src="imagenes/imagenesInicio/NorteMerida.jpg" alt="" class="regiones_ciudad_img"> 
+                </div>
+                <div class="Elemento region">
+                    <h1 class="column__title">Región Noroeste</h1>	
+                        <p>Avenida Mérida 2000 No. 557 por Calle 31'-1 y Calle 11-A'-1, Residencial Pensiones, CP. 97217 Mérida, Yuc.
+                        <br>$2,000.00 mx al mes.
+                        <br>Disponible.</p>
+
+                        <p>Calle 52 No. 300 por Calle 51 y Calle 53, Francisco de Montejo, CP. 97203 Mérida, Yuc.
+                        <br>$5,000.00 mx al mes.
+                        <br>Disponible.</p>
+
+                        <p>Calle 43A No. 324 esquina con Circuito Colonias, Tanlum, CP. 97210 Mérida, Yuc.
+                        <br>$8,000.00 mx al mes.
+                        <br>No disponible actualmente.</p>
+                </div>    
+            </div>
+            <div id="Sureste" class="contenedor  RegionesCarteleras">
+                <div class="Elemento  ">
+                            <img src="imagenes/imagenesInicio/SurMerida.jpg" alt="" class="regiones_ciudad_img">
+                </div>
+                <div class="Elemento region">
+                    <h1 class="column__title">Región Sureste</h1>	
+                        <p>Azalea, Calle 7 No. 177, esquina con Calle 9, Residencial San Carlos, CP. 97130 Mérida, Yuc.
+                        <br>$7,500.00 mx al mes.
+                        <br>No disponible actualmente.</p>
+
+                        <p>Calle No. 19A Diagonal, esquina con Avenida Yucatán, Jardines del Nte. CP. 97139 Mérida, Yuc.
+                        <br>$6,000.00 mx al mes.
+                        <br>Disponible.</p>
+
+                        <p>Av. Andrés García Lavín, San Ramón Nte I, 97117 Mérida, Yuc.
+                        <br>$8,000.00 mx al mes.
+                        <br>Disponible.</p>
+                </div>    
+            </div>
+            <div id="Noreste" class="contenedor container RegionesCarteleras">
+                <div class="Elemento  ">
+                            <img src="imagenes/imagenesInicio/EsteMerida.jpg" alt="" class="regiones_ciudad_img">
+                </div>
+                <div class="Elemento region">
+                    <h1 class="column__title">Región Noreste</h1>	
+                        <p>Calle 69 No. 406, Morelos Oriente, 97174 Mérida, Yuc.
+                        <br>$7,000.00 mx al mes.
+                        <br>Disponible.</p>
+
+                        <p>Calle 42 No. 854 local 7 x 17, Colonia María Luisa, María Luisa, 97199 Mérida, Yuc.
+                        <br>$6,000.00 mx al mes.
+                        <br>Disponible.</p>
+
+                        <p>Cto Colonias 773, Castilla Cámara, 97278 Mérida, Yuc.
+                        <br>$5,000.00 mx al mes.
+                        <br>Disponible.</p>
+                </div>    
+            </div>
+            <div id="Suroeste" class="contenedor RegionesCarteleras">
+                <div class="Elemento  ">
+                            <img src="imagenes/imagenesInicio/OesteMerida.jpg" alt="" class="regiones_ciudad_img">
+                </div>
+                <div class="Elemento region">
+                    <h1 class="column__title">Región Suroeste</h1>	
+                        <p>Oxxo Xoclan, 65 A 1000, Xoclán, 97246 Mérida, Yuc.
+                        <br>$6,000.00 mx al mes.
+                        <br>Disponible.</p>
+
+                        <p>México 261, Cd Industrial, 97288 Mérida, Yuc.
+                        <br>$5,000.00 mx al mes.
+                        <br>Disponible.</p>
+
+                        <p>Calle 16 No. 446D, El Roble Agrícola I, 97255 Mérida, Yuc.
+                        <br>$5,000.00 mx al mes.
+                        <br>Disponible.</p>
+                </div>    
+            </div>
+        </div>
+       
+    </main>
 	
 	<header>
 		<div class="wrapper">
