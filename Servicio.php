0f674cb65353e05cea41e0983caf1ac2c0da7398
Moises <Moises@192.168.1.80>
2018-11-25 20:49:39 -0600

    Agruegué la seccion del header

--- Servicio.php
+++ Servicio.php
@@ -17,7 +17,23 @@ and open the template in the editor.
 </head>
 
 <body>
-	
+
+    <header class="main-header">   
+                    <div class="container container--flex">
+                        <div class ="logo-container column column--50">
+                            <h1 class="logo">Logo</h1>
+                        </div>
+                        <div class="main-header__contactInfo column column--50">
+                            <p class="main-header__contactInfo__phone">
+                                <span class="icon-phone">999-999-999</span>
+                            </p>
+                            <p class="main-header__contactInfo__adress">
+                                <span class="icon-map-marca">Mérida,Yucatán, México</span>
+                            </p>    
+                        </div>    
+                    </div>
+    </header>
+
 	<header>
 		<div class="wrapper" >
 |			<div class ="logo">Publicis </div>
