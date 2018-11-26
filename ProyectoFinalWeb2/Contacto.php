3bdd97f7e0b19404e1e6643c06ffcd07c93d5762
Moises <Moises@192.168.1.80>
2018-11-26 00:49:48 -0600

    Se eliminó el enlace hacia el css estilosMedina y se agregó enlace hacia el css header

--- ProyectoFinalWeb2/Contacto.php
+++ ProyectoFinalWeb2/Contacto.php
@@ -9,7 +9,7 @@ and open the template in the editor.
         <title>Contacto</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styleDaniel.css">
+        <link rel="stylesheet" href="HeaderStyleSheet.css">
         <script src="contact-form-validation.js"></script>
     </head>
     <body>
