ed7840a9a100762fc6c75bdcd8624660fdb6b8e2
fez3d <soydanielmedina@gmail.com>
2018-11-26 15:07:01 -0600

    arregle el nav de contacto y de inicio

--- ProyectoFinalWeb2/Inicio.php
+++ ProyectoFinalWeb2/Inicio.php
@@ -53,26 +53,38 @@ and open the template in the editor.
     			<li class="menu__item">
     				<a href="Contacto.php" class="menu__link "> Contacto</a>
     			</li>
+                        <?php
+                    session_start();
+                    if($_SESSION['tipo_usuario'] == "administrador"){
+                        echo '<li class="menu__item">';
+                        echo '<a href="VistaAdministrador.php" class="menu__link "> Ver Anuncios</a>';
+                        echo '</li>';
+                    } else if ($_SESSION['tipo_usuario'] == "cliente"){
+                        echo '<li class="menu__item">';
+                        echo '<a href="VistaContratar.php" class="menu__link "> Contratar</a>';
+                        echo '</li>';
+                        echo '<li class="menu__item">';
+                        echo '<a href="VistaVerContrataciones.php" class="menu__link "> Ver Mis Contrataciones</a>';
+                        echo '</li>';
+                    }
+
+                    if($_SESSION['inicio'] == null || $_SESSION['inicio'] == false){
+                        echo '<li class="menu__item">';
+                        echo '<a href="IniciarSesion.php" class="menu__link "> Iniciar Sesion</a>';
+                        echo '</li>';
+                    } else{
+                        echo '<li class="menu__item">';
+                        echo '<a href="CerrarSesion.php" class="menu__link "> Cerrar Sesion</a>';
+                        echo '</li>';
+                    }
+                ?>
     		</ul>
     		<div class="social-icon">
     			<a href="" class="social-icon__link"><span class="icon-mail"></span></a> 			
     		</div>
 
     	</div>      
-            <?php
-                if($_SESSION['tipo_usuario'] == "administrador"){
-                    echo '<a href="VistaAdministrador.php"> Ver Anuncios</a>';
-                } else if ($_SESSION['tipo_usuario'] == "cliente"){
-                    echo '<a href="VistaContratar.php"> Contratar</a>';
-                    echo '<a href="VistaVerContrataciones.php"> Ver Mis Contrataciones</a>';
-                }
-
-                if($_SESSION['inicio'] == null || $_SESSION['inicio'] == false){
-                    echo '<a href="IniciarSesion.php"> Iniciar Sesion</a>';
-                } else{
-                    echo '<a href="CerrarSesion.php"> Cerrar Sesion</a>';
-                }
-            ?>
+            
     </nav>
     
     <section class="banner">
