fef3977764906642a4c8a8a8486742f1bfbec644
Moises <Moises@192.168.1.80>
2018-11-25 19:05:34 -0600

    Agregué un nuevo header sin eleminar el anterior

--- Inicio.php
+++ Inicio.php
@@ -21,7 +21,25 @@ and open the template in the editor.
 	
 </head>
 <body>
-	
+
+    <header class="main-header">	
+        <div class="container container--flex">
+            <div class ="logo-container column column--50">
+                <h1 class="logo">Inicio</h1>
+            </div>
+            <div class="main-header__contactInfo column column--50">
+                <p class="main-header__contactInfo__phone">
+                    <span class="icon-phone">999-999-999</span>
+                </p>
+                <p class="main-header__contactInfo__adress">
+                    <span class="icon-map-marca">Mérida,Yucatán, México</span>
+                </p>    
+            </div>    
+        </div>
+    </header>
+    
+
+    
 	<header>
 		<div class="wrapper">
 			<div class ="logo">Publicis </div>
