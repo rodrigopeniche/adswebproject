f42f4da04da3cfd50437617973f0d285e8c1bdcc
fez3d <soydanielmedina@gmail.com>
2018-11-26 22:06:16 -0600

    Todas las paginas tienen el mismo nav

--- ProyectoFinalWeb2/AgregarRadio.php
+++ ProyectoFinalWeb2/AgregarRadio.php
@@ -9,8 +9,13 @@ and open the template in the editor.
         <title>Agregar radio</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styleDaniel.css">
-        <script src="contact-form-validation-Radio.js"></script>
+        <!--<link href="NavbarCss.css" rel="stylesheet" type="text/css"/>-->
+        <link href="estiloVentanasAgregar.css" rel="stylesheet" type="text/css"/>
+        <!--<link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">-->
+        <script src="contact-form-validation-Tv.js"></script>
+        <link rel="stylesheet" href="HeaderStyleSheet.css">
+        <link rel="stylesheet" href="ServiciosStyleSheet.css">
+        <link rel="stylesheet" href="SliderStyleSheet.css">
     </head>
     <body>
         <?php
@@ -18,11 +23,10 @@ and open the template in the editor.
         //include("ValidacionSesionExpirada.php");
             function agregarCartelera(){
                 $baseDatos = new BaseDeDatos();
-                $id = $_POST['id'];
-                $direccion = $_POST['direccion'];
-                $titulo = $_POST['titulo'];
+                $id = $_POST['estacion'];
+                $titulo = $_POST['nombre'];
                 $precio = $_POST['precio'];
-                $query = "INSERT INTO carteleras VALUES ('".$id."','".$direccion."','".$titulo."','".$precio."');" ;
+                $query = "INSERT INTO radio VALUES (".$id.",'".$titulo."',".$precio.");" ;
                 
                 if(is_int($id) && is_int($precio)){
                     $baseDatos->EjecutarQuery($query);
@@ -35,69 +39,92 @@ and open the template in the editor.
             }  
             
         ?>
-        <header>
-        <div class="wrapper">
-|           <div class ="logo">The Masters Of Software </div>
-
-            <nav>
-            
-                <a href="Inicio.html"> Inicio</a>
-                <a href="Nosotros.html"> Nosotros</a>
-                <a href="Servicios.html"> Servicios</a>
-                <a href="Contacto.html"> Contacto</a>
+         <header class="main-header">   
+                    <div class="container container--flex">
+                        <div class ="logo-container column column--50">
+                            <h1 class="logo">Logo</h1>
+                        </div>
+                        <div class="main-header__contactInfo column column--50">
+                            <p class="main-header__contactInfo__phone">
+                                <span class="icon-phone">999-999-999</span>
+                            </p>
+                            <p class="main-header__contactInfo__adress">
+                                <span class="icon-map-marca">Mérida,Yucatán, México</span>
+                            </p>    
+                        </div>    
+                    </div>
+    </header>
+    <nav class="main-nav">
+        <div class="container container--flex">
+            <span class="icon-menu" id="btnmenu"></span>
+            <ul class="menu" id="menu">
+                <li class="menu__item">
+                    <a href="Inicio.php" class="menu__link menu__link--select"> Inicio</a>
+                </li>
+                <li class="menu__item">
+                    <a href="Nosotros.php" class="menu__link"> Nosotros</a>
+                </li>
+                <li class="menu__item">
+                    <a href="Servicio.php" class="menu__link "> Servicios</a>
+                </li>
+                <li class="menu__item">
+                    <a href="Contacto.php" class="menu__link "> Contacto</a>
+                </li>
                 <?php
-                session_start();
-                if($_SESSION['tipo_usuario'] == "administrador"){
-                    echo '<a href="VistaAdministrador.php"> Ver Anuncios</a>';
-                } else if ($_SESSION['tipo_usuario'] == "cliente"){
-                    echo '<a href="VistaContratar.php"> Contratar</a>';
-                    echo '<a href="VistaVerContrataciones.php"> Ver Mis Contrataciones</a>';
-                }
+                    session_start();
+                    if($_SESSION['tipo_usuario'] == "administrador"){
+                        echo '<li class="menu__item">';
+                        echo '<a href="VistaAdministrador.php" class="menu__link "> Ver Anuncios</a>';
+                        echo '</li>';
+                    } else if ($_SESSION['tipo_usuario'] == "cliente"){
+                        echo '<li class="menu__item">';
+                        echo '<a href="VistaContratar.php" class="menu__link "> Contratar</a>';
+                        echo '</li>';
+                        echo '<li class="menu__item">';
+                        echo '<a href="VistaVerContrataciones.php" class="menu__link "> Ver Mis Contrataciones</a>';
+                        echo '</li>';
+                    }
 
-                if($_SESSION['inicio'] == null || $_SESSION['inicio'] == false){
-                    echo '<a href="IniciarSesion.php"> Iniciar Sesion</a>';
-                } else{
-                    echo '<a href="CerrarSesion.php"> Cerrar Sesion</a>';
-                }
+                    if($_SESSION['inicio'] == null || $_SESSION['inicio'] == false){
+                        echo '<li class="menu__item">';
+                        echo '<a href="IniciarSesion.php" class="menu__link "> Iniciar Sesion</a>';
+                        echo '</li>';
+                    } else{
+                        echo '<li class="menu__item">';
+                        echo '<a href="CerrarSesion.php" class="menu__link "> Cerrar Sesion</a>';
+                        echo '</li>';
+                    }
                 ?>
+                
+            </ul>
 
-            </nav>
-        </div>
+            
+        </div>      
+            
+    </nav>
 
+        
 
-        </header>
-
-        <p> Ingrese los siguientes datos de la estación de radio a registrar: <br></p>
-
-        <div class="row">
-            <div class="column">
-                <form name="contact-form">
-                    ID: <br>
-                    <input type="text" size="40" name="id">
+         <div class="add-box">
+              <h1> Ingrese los siguientes datos del canal de televisión a registrar:</h1>
+  
+                <form name="contact-form" method="post">
+                     Estacion: <br>
+                    <input type="text"  name="estacion" placeholder="Ingresa el Canal">
                     <br><br>
                         
-                    Estación: <br>
-                    <input type="text" size="40" name="estacion">
-                    <br><br>
-                        
-                    Título: <br>
-                    <input type="text" size="40" name="titulo">
+                    Nombre: <br>
+                    <input type="text" name="nombre" placeholder="Ingresa el titulo">
                     <br><br>
                     
                     Precio: <br>
-                    <input type="text" size="40" name="precio">
+                    <input type="text"  name="precio" placeholder="Ingresa el precio">
                     <br><br>
                         
-                    <button type="button" onclick="validateForm()">Enviar!</button>                       
+                    <input type="submit"  value="Registrar Datos" onclick="validateForm()">                       
                 </form>
             </div>
-        </div>
-        <footer>
-        <p class="Foot" align="center">
-                                The Masters Of Softwae S.A de C.V&nbsp;&nbsp;
+    </div>
 
-                                Todos los Derechos Reservados
-                    </p>
-    </footer>
     </body>
 </html>
